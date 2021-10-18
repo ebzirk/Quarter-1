f98026a1c124d89f855d4b18773616cca15dfbc7
Bonnie Penrod <ebzirk@gmail.com>
2021-10-18 14:47:30 -0600

    Beginning of my random generator assignment.

---
+++ Random-Number-Generator/src/edu/neumont/Main.java
@@ -0,0 +1,14 @@
+package edu.neumont;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setBounds(200,200,200,200);
+
+
+        frame.setVisible(true);
+    }
+}
