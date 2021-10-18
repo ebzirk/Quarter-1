361c26fa891680738e39b5c1bbf4de4eabe39b26
Bonnie Penrod <ebzirk@gmail.com>
2021-10-18 13:54:09 -0600

    Updated with actual code.

--- Objects/src/edu/neumont/Main.java
+++ Objects/src/edu/neumont/Main.java
@@ -3,6 +3,22 @@ package edu.neumont;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+        System.out.println(add(20,10));
+        print("Hiya!")
     }
+
+    int add(int v1, int v2){
+        int result = v1 + v2;
+        return v1 + v2;
+    }
+
+    static void print(String){
+        System.out.println(s);
+    }
+
+    static double sqr(double v){
+        return v * v;
+    }
+    
 }
