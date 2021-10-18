3d8e13aab03b75328b4b1cb22598faa7b2deb4c9
Bonnie Penrod <ebzirk@gmail.com>
2021-10-18 12:52:42 -0600

    This is the DataTypes example.

---
+++ DataTypes/src/Program.java
@@ -0,0 +1,28 @@
+public class Program {
+    public static void main(String[] args) {
+
+        byte lives = 127;
+        short hitPoints = 32,767;
+        long potato = 12415635122l;
+        double savings = 2131452.23;
+
+        lives = hitPoints;
+
+        int count = -34;
+        float price = 3.0f;
+
+        int iprice = (int)price;
+
+        boolean isDead = false;
+        char initial = 76;
+
+        String name = "Tanner";
+        String fact = "Tanner has 3 siblings";
+
+
+        System.out.println(name);
+        System.out.println(fact);
+        System.out.println(fact);
+
+    }
+}
