880f5138577841ace5fdb19943b4348f926ee205
Bonnie Penrod <ebzirk@gmail.com>
2021-10-18 14:10:54 -0600

    Further updates from class.

--- Objects/src/edu/neumont/Main.java
+++ Objects/src/edu/neumont/Main.java
@@ -1,24 +1,38 @@
 package edu.neumont;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String greeting = scanner.nextLine();
+        int min = Integer.parseInt((greeting));
+        String greeting2 = scanner.nextLine();
+        int max = Integer.parseInt(greeting2);
+
+        Random random = new Random();
+        int r = random.nextInt(min, max);
+
+        String result = Integer.toString(r);
+        System.out.println("Random: " + result);
+
         // write your code here
-        System.out.println(add(20,10));
-        print("Hiya!")
+        System.out.println(add(20, 10));
+        print("Hiya!");
     }
 
-    int add(int v1, int v2){
+    static int add(int v1, int v2) {
         int result = v1 + v2;
         return v1 + v2;
     }
 
-    static void print(String){
-        System.out.println(s);
+    static void print(String greeting) {
+        System.out.println(greeting);
     }
 
-    static double sqr(double v){
+    static double sqr(double v) {
         return v * v;
     }
-    
 }
