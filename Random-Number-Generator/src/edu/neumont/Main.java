45c19f5eb1aa4bffe26830bee8216d0f9b99254f
Bonnie Penrod <ebzirk@gmail.com>
2021-10-19 10:07:23 -0600

    Update from yesterday. Next update should be finished product.

--- Random-Number-Generator/src/edu/neumont/Main.java
+++ Random-Number-Generator/src/edu/neumont/Main.java
@@ -1,14 +1,38 @@
 package edu.neumont;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Main {
 
     public static void main(String[] args) {
         JFrame frame = new JFrame();
-        frame.setBounds(200,200,200,200);
+        frame.setBounds(1000,1000,700,700);
+        frame.setLayout(null);
+
+        JLabel title = new JLabel("Random Number Generator");
+        title.setBounds(150, 80, 1000, 50);
+        title.setFont(new Font("Serif", Font.PLAIN, 20));
+        frame.add(title);
+
+        JTextField minimum = new JTextField();
+        minimum.setBounds(40,200,200,30);
+        frame.add(minimum);
+        JLabel minimumLabel = new JLabel("Minimum");
+        minimumLabel.setBounds(110,250, 80, 30);
+        frame.add(minimumLabel);
+
+        JTextField maximum = new JTextField();
+        maximum.setBounds(350,200,200,30);
+        frame.add(maximum);
+        JLabel maxLabel = new JLabel("Noun");
+        maxLabel.setBounds(350,200, 40, 30);
+        frame.add(maxLabel);
+
+
 
 
         frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
